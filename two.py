07da8bb2679073dac5b535bf1a89d259e796e1f5
kajal-solanki18 <solankikajal5645@gmail.com>
2026-01-06 09:10:46 +0530

    Add files via upload

---
+++ two.py
@@ -0,0 +1,13 @@
+print("hello world")
+x=20
+print(x)
+x=20.5
+print(x)
+x=1j
+print(x)
+x=["apple","banana","cherry"]
+print(x)
+x=frozenset({"apple","banana","cherry"})
+print(x)
+x=True
+print(x)
