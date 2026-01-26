d204150c5251204cf5346f1b9d0c1cc039f096ce
kajal <solankikajal5645@gmail.com>
2026-01-26 19:51:47 +0530

    labtask.py

---
+++ labtask.py
@@ -0,0 +1,44 @@
+"""p=1000
+r=5
+t=2
+sI=(p*r*t)/100
+print("simple Interest is :",sI)"""
+
+"""a=14
+b=23
+if a>b:
+    print("maximum number is:",a)
+else:
+    print("maximum number is:",b)"""
+
+"""for i in range(1,6):
+    print(i)"""
+
+"""text="python"
+lenght=len(text)
+print("lenght of string:",lenght)"""
+
+"""x="welcome"
+print(x)"""
+
+"""text="python"
+print("first character:",text[0])"""
+
+"""text="python"
+print("last charecter:",text[-1])"""
+
+"""num=10
+if num>0:
+    print("number is positive")
+else:
+    print("number is negative")"""
+
+
+"""a=10
+b=20
+c=30
+sum=a+b+c
+print("sum of three number:",sum)"""
+
+num=int(input("Enter a number:"))
+print("you entered:",num)
