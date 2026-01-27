db97d7c6e0ca6dc17d930ba0820538dbd0591dc1
kajal <solankikajal5645@gmail.com>
2026-01-27 23:04:33 +0530

    operator.py

---
+++ operator.py
@@ -0,0 +1,52 @@
+#1.arithmetic operator
+"""a=5
+b=12
+print(a+b)"""
+
+"""a=6
+b=21
+print(a-b)"""
+
+"""a=3
+b=4
+print(a*b)"""
+
+"""a=12
+b=7
+print(a/b)"""
+#2.assignment operator
+"""x=11
+print(x)"""
+
+"""x=10
+print(x)"""
+#3.unary minus
+"""a=6
+print(-a)"""
+#4.relational operators
+"""p=11
+q=10
+print(p>q)
+print(p==q)"""
+#5.logical operators
+"""x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)"""
+#6.boolean operators
+"""is_pass=True
+print(is_pass)"""
+#7.bitwise operators
+"""a=5
+b=3
+print(a&b)
+print(a|b)"""
+#8.membership operators
+"""color=['red','purple','yello']
+print('red'in color)"""
+#9.Identity operators
+"""x=10
+y=10
+print(x is y)
+print(x is not y)"""
