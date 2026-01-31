69a5c0c0397e8f3e590ee4d523b0fe6dd6522dfe
kajal <solankikajal5645@gmail.com>
2026-01-31 08:24:08 +0530

    output.py

---
+++ output.py
@@ -0,0 +1,9 @@
+#.output command
+print("hello world")
+print("welcom to python")
+#.input command
+"""name=input("enter your name:")
+print("hello",name)"""
+
+num=int(input("Enter an integer:"))
+print("number=",num)
