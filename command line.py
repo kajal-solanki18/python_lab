4eb4bad650992525dd89c20d42fcddc15146fd58
kajal <solankikajal5645@gmail.com>
2026-02-22 00:28:59 +0530

    command line.py

---
+++ command line.py
@@ -0,0 +1,18 @@
+"""import sys
+print("number of arguments:",len(sys.argv))
+print("Arguments are:",sys.argv)"""
+
+"""import sys
+print("hello welcome to atmia Argument:",sys.argv)"""
+
+import sys 
+#command line argument 
+num1=int(sys.argv[1])
+num2=int(sys.argv[2])
+sum=num1+num2
+print("addition is:",sum)
+
+
+
+
+
