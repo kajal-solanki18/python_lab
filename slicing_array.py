63185fe5d067a146b74c881913aa10196049ca45
kajal <solankikajal5645@gmail.com>
2026-03-10 22:51:01 +0530

    slicing_array.py

---
+++ slicing_array.py
@@ -0,0 +1,29 @@
+# basic slices
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:])
+print(arr[:])"""
+
+# slicing with step
+"""from array import array
+arr=array('i',[10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])"""
+# negative
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1])
+print(arr[-3:])
+print(arr[:-2])"""
+# reverse
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::-1])"""
+# modifying
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,45])
+print(arr)
