a81990f747e3e39d656feff50c85fa5951b70519
kajal <solankikajal5645@gmail.com>
2026-03-10 22:13:13 +0530

    array.py

---
+++ array.py
@@ -0,0 +1,68 @@
+"""from array  import array
+arr=array('i',[10,20,30,40])
+print(arr)
+print(type(arr))"""
+
+# basic array oprations
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(len(arr))"""
+
+"""from array import array
+arr=array('i',[10,20,30])
+arr.append(40)
+print(arr)"""
+
+"""from array import array
+arr=array('i',[10,20,40])
+arr.insert(2,30)
+print(arr)"""
+
+"""from array import array
+arr=array('i',[10,20,30,20,40])
+arr.remove(20)
+print(arr)"""
+
+"""from array import array
+arr=array('i',[10,20,30,40])
+x=arr.pop()
+print("removed:",x)
+print(arr)"""
+
+"""from array import array
+arr=array('i',[10,20,30,40])
+print(arr.index(30))"""
+
+"""from array import array
+arr=array('i',[10,20,30,20,40])
+print(arr.count(20))"""
+
+"""from array import array
+arr=array('i',[10,20,30,40])
+arr.reverse()
+print(arr)"""
+
+# positive indexing
+from array import array
+"""arr=array('i',[10,20,30,40,50])
+print(arr[0])
+print(arr[2])
+print(arr[4])"""
+
+# negative indexing
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-1])
+print(arr[-2])
+print(arr[-5])"""
+# modifying
+"""from array import array
+arr=array('i',[10,20,30,40,50])
+arr[2]=35
+print(arr)"""
+
+# Error
+from array import array
+arr=array('i',[10,20,30])
+print(arr[5])
+
