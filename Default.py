9f600addd5f820ec32e59461606756d117e2dcee
kajal <solankikajal5645@gmail.com>
2026-03-17 22:09:24 +0530

    Default.py

---
+++ Default.py
@@ -0,0 +1,20 @@
+"""def gree(name,msg="good morning"):
+    print("hello",name +",",msg)
+gree("asha")
+gree("ravi","good evening")"""
+
+#.2
+"""def power(num,exp=2):
+    return num ** exp
+print(power(3))
+print(power(3,3))
+print(power(2,4))"""
+
+#.3
+def student_info(name,age=18,course="BCA"):
+    print("name:",name)
+    print("age:",age)
+    print("course:",course)
+student_info("ravi")
+student_info("seema",20)
+student_info("amit",19,"BSC it")
