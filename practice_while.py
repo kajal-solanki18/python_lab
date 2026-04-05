ebf6ecac6f9a8e0397eb53a4730f98951f96076e
kajal <solankikajal5645@gmail.com>
2026-04-05 20:35:07 +0530

    practice_while.py

---
+++ practice_while.py
@@ -0,0 +1,37 @@
+"""i=1
+while i<=5:
+    print(i)
+    i=i+1"""
+"""n=int(input("Enter a number:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum =",s)"""
+"""i=1
+while i<=20:
+    print(i)
+    i=i+2"""
+"""num=int(input("Enter number :" ))
+i=1
+while i<=10:
+    print(num,"x",i,"=",num*i)
+    i=i+1"""
+"""i=10
+while i>=1:
+    print(i)
+    i=i-1"""
+"""numbers = [10, 25, 7, 89, 45]
+largest = numbers[0]
+i = 0
+while i < len(numbers):
+    if numbers[i] > largest:
+        largest = numbers[i]
+    i = i + 1
+print("Largest number is:", largest)"""
+i = 1
+while i <= 20:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
