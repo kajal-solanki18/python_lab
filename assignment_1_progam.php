57d3e41932827f8c0daba3138a81c12c249d297d
kajal <solankikajal5645@gmail.com>
2026-07-23 10:36:37 +0530

    assingnment_1_progam.php

---
+++ assignment_1_progam.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP HTML Table</title>
+</head>
+<body>
+
+<?php
+echo "<table border='1' cellpadding='10'>";
+
+echo "<tr>";
+echo "<th>Name</th>";
+echo "<th>Roll No</th>";
+echo "<th>Course</th>";
+echo "<th>City</th>";
+echo "<th>Marks</th>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Kajal</td>";
+echo "<td>101</td>";
+echo "<td>BCA</td>";
+echo "<td>Rajkot</td>";
+echo "<td>85</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Riya</td>";
+echo "<td>102</td>";
+echo "<td>BCA</td>";
+echo "<td>Ahmedabad</td>";
+echo "<td>90</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Neha</td>";
+echo "<td>103</td>";
+echo "<td>BCA</td>";
+echo "<td>Surat</td>";
+echo "<td>78</td>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td>Priya</td>";
+echo "<td>104</td>";
+echo "<td>BCA</td>";
+echo "<td>Baroda</td>";
+echo "<td>88</td>";
+echo "</tr>";
+
+echo "</table>";
+?>
+
+</body>
+</html>
