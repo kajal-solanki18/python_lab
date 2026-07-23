d0b78181987bbd95d42aa56d2f89208def8abcf0
kajal <solankikajal5645@gmail.com>
2026-07-23 10:47:43 +0530

    pro_2.php

---
+++ pro_2.php
@@ -0,0 +1,14 @@
+<?php
+$name = "Dipak";
+$contact = "9825298252";
+$address = "University Road";
+$pincode = "360007";
+
+// Display two variables in PHP itself
+echo "Name: " . $name . "<br>";
+echo "Contact: " . $contact . "<br>";
+?>
+
+<!-- Display two variables using Inline PHP -->
+Address: <?php echo $address; ?><br>
+Pincode: <?php echo $pincode; ?>
